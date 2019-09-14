791b0ca10722cb0a746f7da180eb4c7bfda525ec
alihsistu <alihsistu@gmail.com>
2019-09-14 16:29:39 +0800

    fixed bugs

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,30 @@ import 'dart:convert';
 // for making http requests
 import 'package:http/http.dart' as http;
 
-// Wall Street Journal
-// String _urlApi1 =
-//     'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=e3d3b881510f4522a3a0c19fc55489ca';
 
 // Apple news
-String _urlApi2 =
+String _urlApiApple =
     'https://newsapi.org/v2/everything?q=apple&from=2019-09-12&to=2019-09-12&sortBy=popularity&apiKey=e3d3b881510f4522a3a0c19fc55489ca';
 
 // Business headlines in US
-String _urlApi3 =
+String _urlApiBusiness =
     'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e3d3b881510f4522a3a0c19fc55489ca';
 
 // TechCrunch
-String _urlApi4 =
+String _urlApiTech =
     'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e3d3b881510f4522a3a0c19fc55489ca';
 
 // Private response variables
-// List _response1;
-List _response2;
-List _response3;
-List _responseAss;
+List _responseApple;
+List _responseBusiness;
+List _responseTech;
 
 
 // App's main method
 void main() async {
-  // _response1 = await fetchData(_urlApi1);
-  _response2 = await fetchData(_urlApi2);
-  _response3 = await fetchData(_urlApi3);
-  _responseAss = await fetchData(_urlApi4);
+  _responseApple = await fetchData(_urlApiApple);
+  _responseBusiness = await fetchData(_urlApiBusiness);
+  _responseTech = await fetchData(_urlApiTech);
 
 
   runApp(new MaterialApp(
@@ -57,30 +52,13 @@ class Categories extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             // this button takes the user to Bitcoin Widget below(new page)
-            // Button 1
-            // new RaisedButton(
-            //   padding: const EdgeInsets.all(10.0),
-            //   color: Colors.blue,
-            //   onPressed: () {
-            //     Navigator.push(context,
-            //         new MaterialPageRoute(builder: (context) => new News(response: _response1,)));
-            //   },
-            //   child: new Text(
-            //     "Wall Street Journal News",
-            //     style: new TextStyle(
-            //       fontFamily: "Verdana",
-            //       fontSize: 20.0,
-            //       color: Colors.white,
-            //     ),
-            //   ),
-            // ),
-            // Button 2
+            // 1st Button
             new RaisedButton(
               padding: const EdgeInsets.all(10.0),
               color: Colors.blue,
               onPressed: () {
                 Navigator.push(context,
-                    new MaterialPageRoute(builder: (context) => new News(response: _response2,)));
+                    new MaterialPageRoute(builder: (context) => new News(response: _responseApple,)));
               },
               child: new Text(
                 "Apple News",
@@ -91,13 +69,13 @@ class Categories extends StatelessWidget {
                 ),
               ),
             ),
-            // Button 3
+            // 2nd Button
             new RaisedButton(
               padding: const EdgeInsets.all(10.0),
               color: Colors.blue,
               onPressed: () {
                 Navigator.push(context,
-                    new MaterialPageRoute(builder: (context) => new News(response: _response3,)));
+                    new MaterialPageRoute(builder: (context) => new News(response: _responseBusiness,)));
               },
               child: new Text(
                 "US Business Headlines",
@@ -108,13 +86,13 @@ class Categories extends StatelessWidget {
                 ),
               ),
             ),
-            // Button4
+            // 3rd Button
             new RaisedButton(
               padding: const EdgeInsets.all(10.0),
               color: Colors.blue,
               onPressed: () {
                 Navigator.push(context,
-                    new MaterialPageRoute(builder: (context) => new News(response: _responseAss,)));
+                    new MaterialPageRoute(builder: (context) => new News(response: _responseTech,)));
               },
               child: new Text(
                 "TechCrunch",
